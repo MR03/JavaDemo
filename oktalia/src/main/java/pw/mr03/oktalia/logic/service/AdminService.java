dbbf0379f86f72093ef1c47b0aebe51d0a280fb0
linyaqi <mr03web@foxmail.com>
2018-03-23 14:17:34 +0800

    加密

--- oktalia/src/main/java/pw/mr03/oktalia/logic/service/AdminService.java
+++ oktalia/src/main/java/pw/mr03/oktalia/logic/service/AdminService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import pw.mr03.oktalia.entity.Admin;
 import pw.mr03.oktalia.request.RegisterAdminReq;
 import pw.mr03.oktalia.mapper.AdminMapper;
+import pw.mr03.oktalia.utils.algorithm.AES;
+import pw.mr03.oktalia.utils.algorithm.MD5;
 
 import java.util.List;
 
@@ -28,6 +30,9 @@ public class AdminService implements AdminMapper {
      */
     @Override
     public Integer addAdmin(RegisterAdminReq req) {
+        // aes加密并进行md5计算
+        String pwd = MD5.GetMD5Code(AES.GetAESCode(req.getPwd()), true);
+        req.setPwd(pwd);
         Integer admin = adminMapper.addAdmin(req);
         return admin;
     }
--- oktalia/src/test/java/pw/mr03/oktalia/AdminServiceTests.java
+++ oktalia/src/test/java/pw/mr03/oktalia/AdminServiceTests.java
@@ -5,19 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import pw.mr03.oktalia.request.RegisterAdminReq;
 import pw.mr03.oktalia.logic.service.AdminService;
+import pw.mr03.oktalia.request.RegisterAdminReq;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OktaliaApplicationTests {
+public class AdminServiceTests {
 
 	@Autowired
 	private AdminService adminService;
 
 	@Test
 	public void contextLoads() {
-
+		RegisterAdminReq req = new RegisterAdminReq();
+		req.setMobile("3422222222222222");
+		req.setPwd("df");
+		req.setRealname("sdf");
+		Integer admin = adminService.addAdmin(req);
+		System.out.println(admin);
 	}
 
 }
