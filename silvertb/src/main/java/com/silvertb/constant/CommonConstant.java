595b24e17e86afc2892541ef0133517fe94c7523
linyaqi <mr03web@foxmail.com>
2018-09-19 14:45:40 +0800

    change

--- silvertb/src/main/java/com/silvertb/constant/CommonConstant.java
+++ silvertb/src/main/java/com/silvertb/constant/CommonConstant.java
@@ -4,7 +4,7 @@ public class CommonConstant {
     // 开发模式
     public static final String MODE = "dev";
     // api
-    public static final String PREFIX = "api";
-    public static final String V1 = "v1";
+    private static final String PREFIX = "api";
+    private static final String V1 = "v1";
     public static final String API_PATH_V1 = "/" + PREFIX + "/" + V1 + "/";
 }
