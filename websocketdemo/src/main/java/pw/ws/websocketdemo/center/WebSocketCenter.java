f9b8cf88d0ea4f1358915ec4ff2e838dcbe8ddff
linyaqi <mr03web@foxmail.com>
2018-09-21 11:04:33 +0800

    websocket

---
+++ websocketdemo/src/main/java/pw/ws/websocketdemo/center/WebSocketCenter.java
@@ -0,0 +1,77 @@
+package pw.ws.websocketdemo.center;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@ServerEndpoint("/websocket/{sid}")
+@Component
+public class WebSocketCenter {
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
+    //接收sid
+    private String sid = "";
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    private static CopyOnWriteArraySet<WebSocketCenter> webSocketSet = new CopyOnWriteArraySet<>();
+
+    /**
+     * 连接建立成功调用的方法
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("sid") String sid) {
+        this.session = session;
+        webSocketSet.add(this);     //加入set中
+
+        this.sid = sid;
+        try {
+            sendMessage("连接成功");
+        } catch (IOException e) {
+            System.out.println("websocket IO异常");
+        }
+    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        webSocketSet.remove(this);  //从set中删除
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     *
+     * @param message 客户端发送过来的消息
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        //群发消息
+        for (WebSocketCenter item : webSocketSet) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param session
+     * @param error
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    /**
+     * 实现服务器主动推送
+     */
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+}
